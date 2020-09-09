a91e4dbaf98e7d398030a8b714f6c32c4d007cdb
wukailai1 <Wkl662233>
2020-09-09 17:23:59 +0800

    update

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        String a = null;
+        System.out.println(new StringBuilder().append(1).append("_").append(a));
+    }
+}
