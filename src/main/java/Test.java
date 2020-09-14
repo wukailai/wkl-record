d452ab586784bb12df6d8884b3d840ef755be23f
wukailai1 <wukailai1@xiaomi.com>
2020-09-14 21:01:34 +0800

    update

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Test {
     public static void main(String[] args) {
-        String a = null;
-        System.out.println(new StringBuilder().append(1).append("_").append(a));
+        List<Integer> list1 = new ArrayList();
+        list1.add(1);
+        list1.add(2);
+        List<Integer> list2 = new ArrayList();
+        list2.add(2);
+        list2.add(3);
+        List<Integer> allTableList = Stream.of(list1, list2).flatMap(Collection::stream).distinct().limit(3).collect(Collectors.toList());
+        System.out.println(allTableList);
     }
 }
